e24fc37f3cc05912963447202fbc826e6df13b7a
Niels Heirbaut <niels@opilo.nl>
2025-01-20 08:46:07 +0100

    Add outline to HazardZone

--- src/Opilo.HazardZoneMonitor.Domain/Entities/HazardZone.cs
+++ src/Opilo.HazardZoneMonitor.Domain/Entities/HazardZone.cs
@@ -1,14 +1,19 @@
 ﻿using Ardalis.GuardClauses;
+using Opilo.HazardZoneMonitor.Domain.ValueObjects;
 
 namespace Opilo.HazardZoneMonitor.Domain.Entities;
 
 public sealed class HazardZone
 {
     public string Name { get; }
+    public Outline Outline { get; }
 
-    public HazardZone(string name)
+    public HazardZone(string name, Outline outline)
     {
         Guard.Against.NullOrWhiteSpace(name);
+        Guard.Against.Null(outline);
+
         Name = name;
+        Outline = outline;
     }
 }
--- tests/Opilo.HazardZoneMonitor.UnitTests/Domain/HazardZoneTests.cs
+++ tests/Opilo.HazardZoneMonitor.UnitTests/Domain/HazardZoneTests.cs
@@ -1,15 +1,26 @@
-﻿using Opilo.HazardZoneMonitor.Domain.Entities;
+﻿using System.Collections.ObjectModel;
+using Opilo.HazardZoneMonitor.Domain.Entities;
+using Opilo.HazardZoneMonitor.Domain.ValueObjects;
 using Opilo.HazardZoneMonitor.UnitTests.TestUtilities;
 
 namespace Opilo.HazardZoneMonitor.UnitTests.Domain;
 
 public sealed class HazardZoneTests
 {
+    private static readonly Outline s_validOutline = new(new([
+        new Location(0, 0),
+        new Location(4, 0),
+        new Location(4, 4),
+        new Location(0, 4)
+    ]));
+
+    private const string ValidHazardZoneName = "TestHazardZone";
+
     [Fact]
     public void Constructor_WhenNameIsNull_ThrowsArgumentNullException()
     {
         // Act & Assert
-        Assert.Throws<ArgumentNullException>(() => new HazardZone(null!));
+        Assert.Throws<ArgumentNullException>(() => new HazardZone(null!, s_validOutline));
     }
 
     [Theory]
@@ -17,6 +28,24 @@ public sealed class HazardZoneTests
     public void Constructor_WhenNameIsInvalid_ThrowsArgumentException(string invalidName)
     {
         // Act & Assert
-        Assert.Throws<ArgumentException>(() => new HazardZone(invalidName));
+        Assert.Throws<ArgumentException>(() => new HazardZone(invalidName, s_validOutline));
+    }
+
+    [Fact]
+    public void Constructor_WhenOutlineIsNull_ThrowsArgumentNullException()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => new HazardZone(ValidHazardZoneName, null!));
+    }
+
+    [Fact]
+    public void Constructor_WhenValidNameAndOutlineGiven_CreatesInstance()
+    {
+        // Act
+        var hazardZone = new HazardZone(ValidHazardZoneName, s_validOutline);
+
+        // Assert
+        Assert.Equal(ValidHazardZoneName, hazardZone.Name);
+        Assert.Equal(s_validOutline, hazardZone.Outline);
     }
 }
