32821cfc1fe06c8f776009bb2bd51f506cb8dbe8
Niels Heirbaut <niels@opilo.nl>
2025-01-20 07:20:38 +0100

    Initial creation of HazardZone

---
+++ src/Opilo.HazardZoneMonitor.Domain/Entities/HazardZone.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.GuardClauses;
+
+namespace Opilo.HazardZoneMonitor.Domain.Entities;
+
+public sealed class HazardZone
+{
+    public string Name { get; }
+
+    public HazardZone(string name)
+    {
+        Guard.Against.NullOrWhiteSpace(name);
+        Name = name;
+    }
+}
---
+++ tests/Opilo.HazardZoneMonitor.UnitTests/Domain/HazardZoneTests.cs
@@ -0,0 +1,22 @@
+﻿using Opilo.HazardZoneMonitor.Domain.Entities;
+using Opilo.HazardZoneMonitor.UnitTests.TestUtilities;
+
+namespace Opilo.HazardZoneMonitor.UnitTests.Domain;
+
+public sealed class HazardZoneTests
+{
+    [Fact]
+    public void Constructor_WhenNameIsNull_ThrowsArgumentNullException()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => new HazardZone(null!));
+    }
+
+    [Theory]
+    [ClassData(typeof(InvalidNames))]
+    public void Constructor_WhenNameIsInvalid_ThrowsArgumentException(string invalidName)
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => new HazardZone(invalidName));
+    }
+}
