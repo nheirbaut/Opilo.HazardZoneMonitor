4b897e5bf80479860f585c751dff3145381418c1
Niels Heirbaut <niels@opilo.nl>
2026-02-08 15:31:50 +0100

    Refactor person movement feature: change Handler class to sealed and convert Response class to a record for improved immutability

--- src/Opilo.HazardZoneMonitor.Api/Features/PersonTracking/RegisterPersonMovement/Handler.cs
+++ src/Opilo.HazardZoneMonitor.Api/Features/PersonTracking/RegisterPersonMovement/Handler.cs
@@ -1,8 +1,8 @@
-﻿using Opilo.HazardZoneMonitor.Api.Shared.Cqrs;
+using Opilo.HazardZoneMonitor.Api.Shared.Cqrs;
 
 namespace Opilo.HazardZoneMonitor.Api.Features.PersonTracking.RegisterPersonMovement;
 
-public class Handler : ICommandHandler<Command, Response>
+public sealed class Handler : ICommandHandler<Command, Response>
 {
     public Task<Response> Handle(Command command, CancellationToken cancellationToken)
     {
--- src/Opilo.HazardZoneMonitor.Api/Features/PersonTracking/RegisterPersonMovement/Response.cs
+++ src/Opilo.HazardZoneMonitor.Api/Features/PersonTracking/RegisterPersonMovement/Response.cs
@@ -2,7 +2,4 @@ using Opilo.HazardZoneMonitor.Api.Shared.Cqrs;
 
 namespace Opilo.HazardZoneMonitor.Api.Features.PersonTracking.RegisterPersonMovement;
 
-public class Response(Guid personId) : IResponse
-{
-    public Guid PersonId { get; } = personId;
-}
+public sealed record Response(Guid PersonId) : IResponse;
