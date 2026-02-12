db6d38522a16e462081677ffe168d3192a0ac9d5
Niels Heirbaut <niels@opilo.nl>
2026-02-12 15:41:07 +0100

    Add initial implementation for hazard zones endpoint

---
+++ src/Opilo.HazardZoneMonitor.Api/Features/HazardZones/GetHazardZones/Feature.cs
@@ -0,0 +1,19 @@
+using Opilo.HazardZoneMonitor.Api.Shared.Features;
+
+namespace Opilo.HazardZoneMonitor.Api.Features.HazardZones.GetHazardZones;
+
+public sealed class Feature: IFeature
+{
+    public void AddServices(IServiceCollection services, IConfiguration configuration)
+    {
+        // Nothing to add here yet
+    }
+
+    public void MapEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/v1/hazard-zones", () =>
+        {
+            return TypedResults.Ok();
+        });
+    }
+}
