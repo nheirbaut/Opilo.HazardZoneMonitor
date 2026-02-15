78010573f434ba54a169fe45b129777b8b3d6db2
Niels Heirbaut <niels@opilo.nl>
2026-02-15 20:31:48 +0100

    Add constructor test for Site to validate empty floors collection

--- tests/Opilo.HazardZoneMonitor.Domain.Tests.Unit/Domain/SiteTests.cs
+++ tests/Opilo.HazardZoneMonitor.Domain.Tests.Unit/Domain/SiteTests.cs
@@ -1,15 +1,18 @@
 using Ardalis.GuardClauses;
+using Opilo.HazardZoneMonitor.Domain.Features.FloorManagement.Domain;
 using Opilo.HazardZoneMonitor.Domain.Tests.Unit.TestUtilities;
 
 namespace Opilo.HazardZoneMonitor.Domain.Tests.Unit.Domain;
 
 public sealed class SiteTests
 {
+    private const string ValidSiteName = "TestSite";
+
     [Fact]
     public void Constructor_ShouldThrowArgumentNullException_WhenNameIsNull()
     {
         // Act & Assert
-        var act = () => new Site(null!);
+        var act = () => new Site(null!, []);
         act.Should().Throw<ArgumentNullException>();
     }
 
@@ -18,8 +21,18 @@ public sealed class SiteTests
     public void Constructor_ShouldThrowArgumentException_WhenNameIsInvalid(string invalidName)
     {
         // Act & Assert
-        var act = () => new Site(invalidName);
+        var act = () => new Site(invalidName, []);
         act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Constructor_ShouldCreateSite_WhenEmptyFloorsCollection()
+    {
+        // Act
+        var site = new Site(ValidSiteName, []);
+
+        // Assert
+        site.Name.Should().Be(ValidSiteName);
     }
 }
 
@@ -27,8 +40,12 @@ public sealed class SiteTests
 public sealed class Site
 #pragma warning restore MA0048
 {
-    public Site(string name)
+    public Site(string name, IList<Floor> floors)
     {
         Guard.Against.NullOrWhiteSpace(name);
+
+        Name = name;
     }
+
+    public string Name { get; }
 }
