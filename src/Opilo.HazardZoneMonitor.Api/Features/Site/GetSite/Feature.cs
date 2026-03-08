83bd8ab9723df6f3c7458e68f73221b6c4f4896f
Niels Heirbaut <github@heirbaut.nl>
2026-03-08 08:00:55 +0100

    Implement site configuration retrieval through API endpoint

--- src/Opilo.HazardZoneMonitor.Api/Features/Site/GetSite/Feature.cs
+++ src/Opilo.HazardZoneMonitor.Api/Features/Site/GetSite/Feature.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Options;
 using Opilo.HazardZoneMonitor.Api.Shared.Features;
 
 namespace Opilo.HazardZoneMonitor.Api.Features.Site.GetSite;
@@ -6,11 +7,16 @@ public sealed class Feature : IFeature
 {
     public void AddServices(IServiceCollection services, IConfiguration configuration)
     {
-        // No services to register.
+        services.Configure<SiteOptions>(configuration.GetSection(nameof(SiteOptions)));
     }
 
     public void MapEndpoints(IEndpointRouteBuilder app)
     {
-        app.MapGet("/api/v1/site", () => Results.Ok());
+        app.MapGet("/api/v1/site", (IOptions<SiteOptions> options) =>
+        {
+            var siteOptions = options.Value;
+            var site = new SiteConfiguration(siteOptions.Name ?? string.Empty);
+            return Results.Ok(new Response(site));
+        });
     }
 }
