68e3416e56cd1147f56d747a4808f4f411cb0407
Niels Heirbaut <github@heirbaut.nl>
2026-05-02 18:38:04 +0200

    Add test to validate exception thrown for empty hazard zone name

--- tests/Opilo.HazardZoneMonitor.Tests.Integration/Features/HazardZones/HazardZoneConfigurationStartupSpecification.cs
+++ tests/Opilo.HazardZoneMonitor.Tests.Integration/Features/HazardZones/HazardZoneConfigurationStartupSpecification.cs
@@ -1,4 +1,8 @@
 using System.Net;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Opilo.HazardZoneMonitor.Api.Features.HazardZones;
+using Opilo.HazardZoneMonitor.Api.Shared.Configuration;
 using Opilo.HazardZoneMonitor.Tests.Integration.Shared;
 
 namespace Opilo.HazardZoneMonitor.Tests.Integration.Features.HazardZones;
@@ -19,5 +23,29 @@ public sealed class HazardZoneConfigurationStartupSpecification(CustomWebApplica
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public void Api_ShouldThrowOptionsValidationException_WhenHazardZoneNameIsEmpty()
+    {
+        // Arrange
+        var hazardZoneOptions = new HazardZoneOptions
+        {
+            HazardZones = [new HazardZoneConfiguration(string.Empty, [new PointConfiguration(0, 0), new PointConfiguration(1, 0), new PointConfiguration(0, 1)], TimeSpan.Zero, TimeSpan.Zero)]
+        };
+
+        var customFactory = factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureAppConfiguration((_, config) =>
+            {
+                config.AddInMemoryCollection(hazardZoneOptions.ToConfigurationDictionary());
+            });
+        });
+
+        // Act
+        Action act = () => customFactory.CreateClient();
+
+        // Assert
+        act.Should().Throw<OptionsValidationException>();
     }
 }
