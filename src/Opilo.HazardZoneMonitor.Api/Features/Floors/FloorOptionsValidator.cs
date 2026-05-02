f893e97b2e6981d4043b86d4901713c780b1f818
Niels Heirbaut <github@heirbaut.nl>
2026-05-02 11:50:48 +0200

    Add FloorOptionsValidator to validate floor configuration options

---
+++ src/Opilo.HazardZoneMonitor.Api/Features/Floors/FloorOptionsValidator.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.Options;
+
+namespace Opilo.HazardZoneMonitor.Api.Features.Floors;
+
+public sealed class FloorOptionsValidator : IValidateOptions<FloorOptions>
+{
+    public ValidateOptionsResult Validate(string? name, FloorOptions options)
+    {
+        return ValidateOptionsResult.Success;
+    }
+}
