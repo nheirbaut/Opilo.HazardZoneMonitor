5a3e7d68b431c9c56c8d722a4704c22e7290f3d4
Niels Heirbaut <github@heirbaut.nl>
2026-05-03 15:52:49 +0200

    Add validation test for FloorName value object to ensure invalid names throw exceptions

--- tests/Opilo.HazardZoneMonitor.Domain.Tests.Unit/ValueObjects/FloorNameTests.cs
+++ tests/Opilo.HazardZoneMonitor.Domain.Tests.Unit/ValueObjects/FloorNameTests.cs
@@ -1,4 +1,5 @@
 using Opilo.HazardZoneMonitor.Domain.Shared.ValueObjects;
+using Opilo.HazardZoneMonitor.Domain.Tests.Unit.TestUtilities;
 
 namespace Opilo.HazardZoneMonitor.Domain.Tests.Unit.ValueObjects;
 
@@ -15,5 +16,16 @@ public sealed class FloorNameTests
 
         // Assert
         floorName.Value.Should().Be("GROUNDFLOOR");
+    }
+
+    [Theory]
+    [ClassData(typeof(InvalidNames))]
+    public void From_ShouldThrowValueObjectValidationException_WhenNameIsInvalid(string invalidName)
+    {
+        // Act
+        var act = () => FloorName.From(invalidName);
+
+        // Assert
+        act.Should().Throw<Vogen.ValueObjectValidationException>();
     }
 }
