3c0eb66afa3edc3963a63d2ba96e03b7e724ec7e
Niels Heirbaut <github@heirbaut.nl>
2026-05-03 13:03:42 +0200

    Add unit tests for PersonId value object creation

---
+++ tests/Opilo.HazardZoneMonitor.Domain.Tests.Unit/Shared/ValueObjects/PersonIdTests.cs
@@ -0,0 +1,19 @@
+using Opilo.HazardZoneMonitor.Domain.Shared.ValueObjects;
+
+namespace Opilo.HazardZoneMonitor.Domain.Tests.Unit.Shared.ValueObjects;
+
+public sealed class PersonIdTests
+{
+    [Fact]
+    public void From_ShouldCreatePersonId_WhenGuidIsValid()
+    {
+        // Arrange
+        var guid = Guid.NewGuid();
+
+        // Act
+        var personId = PersonId.From(guid);
+
+        // Assert
+        personId.Value.Should().Be(guid);
+    }
+}
