92443f396ccb24f5f017af18cd3a1c7d713c9645
Niels Heirbaut <github@heirbaut.nl>
2026-05-23 10:53:40 +0200

    Apply actual HazardZoneService

--- tests/Opilo.HazardZoneMonitor.Api.Tests.Unit/Features/HazardZones/ActivateHazardZone/HandlerTests.cs
+++ tests/Opilo.HazardZoneMonitor.Api.Tests.Unit/Features/HazardZones/ActivateHazardZone/HandlerTests.cs
@@ -1,7 +1,7 @@
 using Ardalis.Result;
 using NSubstitute;
-using Opilo.HazardZoneMonitor.Api.Features.HazardZones;
 using Opilo.HazardZoneMonitor.Api.Features.HazardZones.ActivateHazardZone;
+using Opilo.HazardZoneMonitor.Api.Features.HazardZones.Services;
 using Opilo.HazardZoneMonitor.Domain.Shared.Primitives;
 
 namespace Opilo.HazardZoneMonitor.Api.Tests.Unit.Features.HazardZones.ActivateHazardZone;
@@ -12,7 +12,11 @@ public sealed class HandlerTests
     public async Task Handle_ShouldReturnNotFoundResult_WhenNoHazardZonesAreConfigured()
     {
         // Arrange
-        var hazardZoneService = new HazardZoneService();
+        var hazardZoneService = Substitute.For<IHazardZoneService>();
+        hazardZoneService
+            .ActivateHazardZone(Arg.Any<HazardZoneName>())
+            .Returns(Result.NotFound());
+
         Handler handler = new(hazardZoneService);
         Command command = new(HazardZoneName.From("non-existing-hazardzone"));
 
